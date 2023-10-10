4778dd15abf41b66a7d691aa0a8a422b3ee280a1
tu.dinh <tu.dinh@metropolia.fi>
2023-10-10 21:40:17 +0300

    feat: Add player record to database, search continent by country name in database

--- main_game.py
+++ main_game.py
@@ -1,35 +1,75 @@
+import mysql.connector
 
+#user example is a tuple of (name, co2_budget, location, game score)
+user_example = ('Tu', 10000, 'Finland', 0)
+user_example_2 = ('Minh', 10000, 'Sweden', 3)
+
+def get_continent(country):
+    sql = f"SELECT continent FROM country WHERE name = '{country}';"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    database_result = cursor.fetchall()
+    continent = database_result[0][0]
+    return continent #return a list of a tuple
+
+def add_result_database(player):
+    sql = "INSERT INTO player_record (player_name, co2_budget, location, score) VALUES (%s, %s, %s, %s)"
+    value_to_add = player
+    cursor = connection.cursor()
+    cursor.execute(sql, value_to_add)
+    connection.commit()
+
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='dbuser',
+         password='pass_word',
+         autocommit=True
+         )
 
 def main_game():
     name = input("Enter your name: ")
+
     location = input("Which country are you in right now?: ")
+    current_continent = get_continent(location)
+
     flight_count = 0
     co2_budget = 10000
-    print(f"Your current co2 budget: {co2_budget}")
+    print(f"Your current co2 budget: {co2_budget}, continent: {current_continent}")
+
     destination = input("Next destination: ")
+    next_continent = get_continent(destination)
+
     while (co2_budget >= 1000):
         if co2_budget >= 3000:
-            if destination != location:
+            if next_continent != current_continent:
                 co2_budget -= 3000
             else:
                 co2_budget -= 1000
             flight_count +=1
+            current_continent = next_continent
             location = destination
-            print(f"Current value: {co2_budget}")
+            print(f"Current budget: {co2_budget}, continent: {current_continent}")
+
             destination = input("Next destination: ")
+            next_continent = get_continent(destination)
 
         if co2_budget < 3000:
-            if destination != location:
+            if next_continent != current_continent:
                 print("WARNING: You don't have enough co2 budget to make this flight!")
                 break
             else:
                 co2_budget -= 1000
                 flight_count +=1
+                current_continent = next_continent
                 location = destination
                 if co2_budget == 0:
                     break
-            print(f"Current value: {co2_budget}")
+            print(f"Current budget: {co2_budget}, continent: {current_continent}")
+
             destination = input("Next destination: ")
+            next_continent = get_continent(destination)
 
     print(f"Game over, your final score is {flight_count}. Current location: {location}")
     result = name, co2_budget, location, flight_count
@@ -37,7 +77,10 @@ def main_game():
     return result
 
 
-main_game()
+# def updatePlayerToDatabase():
+
+
+
 
 
 
